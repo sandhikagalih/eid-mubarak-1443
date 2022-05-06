7f3a04892e37a65557821d091968943f065489a4
sandhikagalih <sandhikagalih@gmail.com>
2022-05-06 09:41:45 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        almarai: ['Almarai'],
+        poppins: ['Poppins'],
+      },
+    },
+  },
+  plugins: [],
+};
